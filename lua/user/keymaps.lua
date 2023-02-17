596b95cbc27436484c217c2337238e8d2774a0dd
Ken Ng <kenngsimply@gmail.com>
2023-02-17 09:42:17 +0800

    fix: deprecated warning

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -7,8 +7,8 @@ local keymap = vim.api.nvim_set_keymap
 
 --Remap space as leader key
 keymap("", "<Space>", "<Nop>", opts)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
 
 -- Modes
 --   normal_mode = "n",
@@ -36,8 +36,8 @@ keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- Move text up and down
-keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
-keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+-- keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+-- keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
 
 -- Insert --
 -- Press jk fast to enter
@@ -70,3 +70,5 @@ keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 -- Nvimtree
 keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
 keymap("n", "<leader>f", ":Format<cr>", opts)
+keymap("n", "<leader>tt", ":Telescope find_files<cr>", opts)
+keymap("n", "<leader>tg", ":Telescope live_grep<cr>", opts)
--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -36,20 +36,19 @@ local tree_cb = nvim_tree_config.nvim_tree_callback
 nvim_tree.setup {
   disable_netrw = true,
   hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = {
-    "startify",
-    "dashboard",
-    "alpha",
-  },
-  auto_close = true,
+  ---- open_on_setup = false,
+  ---- ignore_ft_on_setup = {
+  ----   "startify",
+  ----   "dashboard",
+  ----   "alpha",
+  ---- },
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = true,
-  update_to_buf_dir = {
-    enable = true,
-    auto_open = true,
-  },
+  ---- update_to_buf_dir = {
+  ----  enable = true,
+  ----  auto_open = true,
+  ----},
   diagnostics = {
     enable = true,
     icons = {
@@ -79,10 +78,10 @@ nvim_tree.setup {
   },
   view = {
     width = 30,
-    height = 30,
+    ---- height = 30,
     hide_root_folder = false,
     side = "left",
-    auto_resize = true,
+    ---- auto_resize = true,
     mappings = {
       custom_only = false,
       list = {
@@ -98,15 +97,15 @@ nvim_tree.setup {
     cmd = "trash",
     require_confirm = true,
   },
-  quit_on_open = 0,
-  git_hl = 1,
-  disable_window_picker = 0,
-  root_folder_modifier = ":t",
-  show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-    tree_width = 30,
-  },
+  ---- quit_on_open = 0,
+  ---- git_hl = 1,
+  --- disable_window_picker = 0,
+  ---- root_folder_modifier = ":t",
+  ---- show_icon = {
+  ----   git = 1,
+  ----   folders = 1,
+  ----   files = 1,
+  ----   folder_arrows = 1,
+  ----   tree_width = 30,
+  ---- },
 }
